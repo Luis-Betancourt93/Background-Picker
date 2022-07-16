5fc02b908ddbf091aab961294126f32c22ebf8f3
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-07-16 09:30:10 -0700

    Background-Picker
    Added some picture, got the first 2 to work but still need to work out the Css

---
+++ js/preview.js
@@ -0,0 +1,40 @@
+function firstTry() {
+  document.body.style.backgroundImage = "url('img/space.jpeg')";
+}
+
+function secondTry() {
+  document.body.style.backgroundImage = "url('http://placeimg.com/200/200/people')";
+
+  // This will not repeat the img
+  // document.body.style.backgroundRepeat = "no-repeat";
+
+}
+
+
+
+
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#img2").addEventListener("click", secondTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
+document.querySelector("#picture").addEventListener("click", firstTry );
+
